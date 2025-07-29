86fc49278e4f01cffbc3b5264a84368f7255afc1
MmaGod1 <gmmaduchukwu@gmail.com>
2025-07-29 14:40:11 +0100

    format file woth npx prettier

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,32 +7,30 @@ const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 const enlargeBtn = player.querySelector(".fullscreen");
 
-
 function play() {
-    video.paused ? video.play() : video.pause();
+  video.paused ? video.play() : video.pause();
 }
 
 function updateButton() {
-    const icon = this.paused ? "►" : "❚ ❚";
-    toggle.textContent = icon;
-    //console.log(icon);
-
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+  //console.log(icon);
 }
 
 function skip() {
-   // console.log(this.dataset);
-   video.currentTime += parseFloat(this.dataset.skip);
+  // console.log(this.dataset);
+  video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function upadateRange() {
-    // console.log(this);
-    // console.log(this.name, this.value);
-    video[this.name] = this.value;
+  // console.log(this);
+  // console.log(this.name, this.value);
+  video[this.name] = this.value;
 }
 
 function updateProgressBar() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 function scrub(event) {
@@ -42,31 +40,32 @@ function scrub(event) {
 }
 
 function fullscreen() {
-    if (!document.fullscreenElement) {
-        player.requestFullscreen();
-    } else {
-        document.exitFullscreen();
-    }
+  if (!document.fullscreenElement) {
+    player.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
 }
 
 function updateFullscreenIcon() {
-  enlargeBtn.textContent = document.fullscreenElement ? "fullscreen_exit" : "fullscreen";
+  enlargeBtn.textContent = document.fullscreenElement
+    ? "fullscreen_exit"
+    : "fullscreen";
 }
 
 video.addEventListener("clic}k", play);
 toggle.addEventListener("click", play);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
-video.addEventListener("timeupdate", updateProgressBar)
+video.addEventListener("timeupdate", updateProgressBar);
 
-skipButtons.forEach(button => button.addEventListener("click", skip));
+skipButtons.forEach((button) => button.addEventListener("click", skip));
 enlargeBtn.addEventListener("click", fullscreen);
 document.addEventListener("fullscreenchange", updateFullscreenIcon);
-ranges.forEach(range => range.addEventListener("change", upadateRange));
+ranges.forEach((range) => range.addEventListener("change", upadateRange));
 
 let mousedown = false;
 progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub);
-progress.addEventListener("mousedown", () => mousedown = true);
-progress.addEventListener("mouseup", () => mousedown = false);
-
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
